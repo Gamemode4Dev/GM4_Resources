0a00e22f670c99724a12f56efdf965af7bdde2ed
SpecialBuilder <specialbuilder32@gmail.com>
2021-08-06 20:15:45 -0400

    Prefixing CMD overwrites and failback models
    -Adds overrides for both legacy and prefixed 342xxxx CMD values.
    -If a model specified by the google-doc does not exist; creates a placeholder model that redirects to the vanilla texture

--- scripts/custom_model_data.py
+++ scripts/custom_model_data.py
@@ -1,8 +1,10 @@
 import pandas
 import urllib.request
 import json
+import os.path
 
 OUT_PATH = "gm4_resources/assets/minecraft/models/item"
+GM4_MODEL_PATH = "gm4_resources/assets/gm4/models"
 
 DOC_ID = "1myt9FkMYkvyzpr9Uu7wXyzXNNreLQreWfWfP4CAGMQM"
 DOC_SHEET = "Data"
@@ -17,6 +19,10 @@ CATEGORIES = {
 	"icon": "gm4:gui/advancements/icon/",
 }
 
+IGNORES = ("fishing_rod", "elytra", "crossbow", "compass", "bow", "clock", "carrot_on_a_stick", "warped_fungus_on_a_stick") # items with strange model files 
+
+CMD_PREFIXS = (0, 3420000) # legacy and registered prefixes
+
 def add_override(i, model):
 	return { "predicate": {"custom_model_data": i }, "model": model }
 
@@ -24,16 +30,32 @@ data = pandas.read_csv(DOC_URL)
 models = json.load(urllib.request.urlopen(MODELS_URL))
 
 for item, overrides in data.groupby("Item"):
-	model = models[item]
-	model["overrides"] = []
-	prev = 0
-	for _, row in overrides.iterrows():
-		if prev > 0 and row[1] - prev > 1:
-			model["overrides"].append(add_override(prev + 1, f"minecraft:item/{item}"))
-		model["overrides"].append(add_override(row[1], CATEGORIES[row[2]] + row[3]))
-		prev = row[1]
-	model["overrides"].append(add_override(prev + 1, f"minecraft:item/{item}"))
+	if item not in IGNORES:
+		model = models[item]
+		model["overrides"] = []
+		prev = 0
+		for prefix in CMD_PREFIXS: # for each listed prefix, legacy and registered
+			for _, row in overrides.iterrows():
+				if prev > 0 and row[1] - prev > 1: # if non-continious CMD range, ensure non-overwridden items default to vanilla
+					model["overrides"].append(add_override(prev + 1 + prefix, f"minecraft:item/{item}"))
 
-	with open(f"{OUT_PATH}/{item}.json", "w") as f:
-		json.dump(model, f, indent=4)
-		f.write("\n")
+				gm4_model_name = CATEGORIES[row[2]] + row[3]
+				model["overrides"].append(add_override(row[1] + prefix, gm4_model_name)) # add override to model file
+		
+				# check if referenced model name exists in gm4_resources, and if not, create a placeholder file
+				gm4_model_file = GM4_MODEL_PATH+gm4_model_name.replace("gm4:", "/")+".json"
+				if not os.path.isfile(gm4_model_file):
+					print(f"model {gm4_model_file} does not exist")
+					with open(gm4_model_file, "w") as f:
+						default_model = { "parent": f"item/{item}" }
+						json.dump(default_model, f, indent=4)
+						f.write("\n")
+						print(f"default {gm4_model_file} created")
+
+				prev = row[1]
+			model["overrides"].append(add_override(prev + 1 + prefix, f"minecraft:item/{item}"))
+
+
+		with open(f"{OUT_PATH}/{item}.json", "w") as f:
+			json.dump(model, f, indent=4)
+			f.write("\n")
