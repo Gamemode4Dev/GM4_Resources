73b00339629f4b7c985e8cd2bbf7db9a3ba1bc2e
Jep Parcels <31190190+jpeterik12@users.noreply.github.com>
2021-11-27 13:37:24 -0600

    a bit more seperation, to aid in future additions

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,41 +1,50 @@
+import pandas
+
 TOOL_MATERIALS=("wooden","stone","iron","golden","diamond","netherite")
 ARMOR_MATERIALS=("leather","chainmail","iron","golden","diamond","netherite")
 
 def preprocess_data(data):
-  to_append = []
-  to_drop = []
+  newData = pandas.DataFrame(columns=["Item","Index","Category","Module","Name","Model","Texture","Parent"])
   for i, row in data.iterrows():
+    if any(isinstance(string, str) and '{' in string for string in row):
+      newData = newData.append(specialHandler(row))
+    else:
+      newData = newData.append([row])
+  return newData
+
+
+def specialHandler(ogrow):
+  extra_rows = [ogrow]
+  for row in extra_rows:
     [item, index, category, module, name, model_type, texture, parent] = row
     if ('{tooltype}' in item):
-        if i not in to_drop: to_drop.append(i)
-        for tool_type in TOOL_MATERIALS:
-          to_append.append({
-            "Item": item.format(tooltype=tool_type),
-            "Index": index, 
-            "Category": category,
-            "Module": module,
-            "Name": name.format(tooltype=tool_type),
-            "Model": model_type,
-            "Texture": texture.format(tooltype=tool_type),
-            "Parent": parent
-          })
+      for tool_type in TOOL_MATERIALS:
+        extra_rows.append({
+          "Item": item.format(tooltype=tool_type),
+          "Index": index, 
+          "Category": category,
+          "Module": module,
+          "Name": name.format(tooltype=tool_type),
+          "Model": model_type,
+          "Texture": texture.format(tooltype=tool_type),
+          "Parent": parent
+        })
     if ('{armortype}' in item):
-        if i not in to_drop: to_drop.append(i)
-        for armor_type in ARMOR_MATERIALS:
-          temptexture = texture
-          if (armor_type == 'netherite') and ('helmet' in item):
-            temptexture += '_netherite'
-          if (armor_type == 'leather') and ('leggings' in item):
-            temptexture += '_leather'
-          to_append.append({
-            "Item": item.format(armortype=armor_type),
-            "Index": index, 
-            "Category": category,
-            "Module": module,
-            "Name": name.format(armortype=armor_type),
-            "Model": model_type,
-            "Texture": temptexture.format(armortype=armor_type),
-            "Parent": parent
-          })
-  data = data.drop(to_drop)
-  return data.append(to_append)
+      for armor_type in ARMOR_MATERIALS:
+        temptexture = texture
+        if (armor_type == 'netherite') and ('helmet' in item):
+          temptexture += '_netherite'
+        if (armor_type == 'leather') and ('leggings' in item):
+          temptexture += '_leather'
+        extra_rows.append({
+          "Item": item.format(armortype=armor_type),
+          "Index": index, 
+          "Category": category,
+          "Module": module,
+          "Name": name.format(armortype=armor_type),
+          "Model": model_type,
+          "Texture": temptexture.format(armortype=armor_type),
+          "Parent": parent
+        })
+    return extra_rows
+  
