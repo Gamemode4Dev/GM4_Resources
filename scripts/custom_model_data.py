c2ac088b0225ddc8f677f8eeb3fb51cceaffe7ad
Misode <misoloo64@gmail.com>
2023-05-15 23:24:32 +0200

    Update pack_format and other versions

--- scripts/custom_model_data.py
+++ scripts/custom_model_data.py
@@ -7,7 +7,7 @@ import special_cases
 import generate_gm4
 import preprocess
 
-VERSION = "1.19"
+VERSION = "1.19.4"
 NAMESPACE = "gm4"
 CMD_PREFIXES = (0, 3420000) # legacy and registered prefixes
 
